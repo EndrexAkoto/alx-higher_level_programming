2c09a45f0434d16b4534550d8abb467b2ed361d2
EndrexAkoto <martinakoto3@gmail.com>
2023-08-18 06:08:30 -0700

    0-privileges.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,7 +5,7 @@ USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
   id int(11) DEFAULT NULL,
   name varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Convert the table to utf8mb4 collate utf8mb4_unicode_ci
 ALTER TABLE first_table
@@ -18,4 +18,3 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Confirm the changes
 SHOW CREATE TABLE first_table;
-
--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges of users in server
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
-
+-- Privileges list of the sql user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
